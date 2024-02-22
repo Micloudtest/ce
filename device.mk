a5a63fff4dd0c659621b887916975387d79a38a8
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-22 16:30:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -25,14 +25,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# API
-PRODUCT_SHIPPING_API_LEVEL := 29
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
 
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
-    ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # fastbootd
 PRODUCT_PACKAGES += \
