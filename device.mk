22ead65703decd98bfdf0948a2db3a4799fa7d09
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-23 02:50:06 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@
 
 LOCAL_PATH := device/xiaomi/lancelot
 
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
