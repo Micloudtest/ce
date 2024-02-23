eaac94df208cc664236b5b40dd5cf85113569985
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-23 23:14:58 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,7 +141,8 @@ TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_FUSE_NTFS  := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_HAS_MTP := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 RECOVERY_SDCARD_ON_DATA := true
 
 # Removes the loop block errors after flashing ZIPs (Workaround) 
@@ -153,6 +154,7 @@ TW_INCLUDE_FB2PNG := true
 
 # USB Configuration
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_HAS_MTP := true
 
 # Log
 TWRP_INCLUDE_LOGCAT := true
