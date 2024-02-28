f0207c5923d625d14c8f507e6017bbb5a87a662f
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-29 05:50:53 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -40,82 +40,80 @@ fi
 if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 	echo -e "\x1b[96m[INFO]: Setting up OrangeFox build vars for fleur...\x1b[m"
 	if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	  	# Version / Maintainer infos
-		export OF_MAINTAINER="Tapin Recovery Instraller"
-		export FOX_VERSION=R12.1_1
-		export FOX_BUILD_TYPE="Unofficial"
-    		export FOX_TARGET_DEVICES="fleur"
-	  	export TARGET_DEVICE_ALT="fleur"
-	  	export LC_ALL="C"
+	  	
+# Version / Maintainer infos
+export OF_MAINTAINER="Tapin Recovery Instraller"
+export FOX_VERSION=R12.1_0
+export FOX_BUILD_TYPE="Unofficial"
+export TARGET_DEVICE_ALT="lancelot"
+export FOX_TARGET_DEVICES="lancelot,shiva"
 
-	 	# Magiskboot
-	  	export OF_USE_MAGISKBOOT=1
-	 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-  
-		# OTA / DM-Verity / Encryption
- 		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-		
-		export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-		export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-		export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-		export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+export TW_DEFAULT_LANGUAGE="en"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+export OF_QUICK_BACKUP_LIST="/boot;/data;"
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
 
-		# MediaTek
-    		export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
+# Magiskboot
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 
-	  	# Display / Leds
-		export OF_SCREEN_H="2400"
-		export OF_STATUS_H="100"
-		export OF_STATUS_INDENT_LEFT=48
-		export OF_STATUS_INDENT_RIGHT=48
-		export OF_HIDE_NOTCH=1
-		export OF_CLOCK_POS=1 # left and right clock positions available
-		export OF_USE_GREEN_LED=0
-		export OF_FLASHLIGHT_ENABLE=1
-		export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.mt6768.rc for more information
+# OTA
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+export OF_NO_MIUI_PATCH_WARNING=1
 
-                # Ensure that /sdcard is bind-unmounted before f2fs data repair or format
- 		export OF_UNBIND_SDCARD_F2FS=1
+# Binaries
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_GREP_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_BRX_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_REPLACE_TOOLBOX_GETPROP=1
+export FOX_USE_XZ_UTILS=1
+export FOX_REPLACE_BUSYBOX_PS=1
+export OF_ENABLE_LPTOOLS=1
 
-	 	# Metadata encription
-   		export OF_FBE_METADATA_MOUNT_IGNORE=1
+# Ensure that /sdcard is bind-unmounted before f2fs data repair or format
+export OF_UNBIND_SDCARD_F2FS=1
 
-	  	# Removes the loop block errors after flashing ZIPs (Workaround) 
-		export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
-		export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
-  
-	  	# Other OrangeFox configs
-		export OF_ENABLE_LPTOOLS=1
-		export OF_ALLOW_DISABLE_NAVBAR=0
-		export FOX_DELETE_AROMAFM=1
-		export FOX_USE_TAR_BINARY=1
-		export FOX_USE_SED_BINARY=1
-		export FOX_USE_XZ_UTILS=1
-                
-	        # Backup
-		export OF_USE_TWRP_SAR_DETECT=1
-		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
-		export OF_QUICK_BACKUP_LIST="/boot;/data;"
-		export FOX_REPLACE_TOOLBOX_GETPROP=1
-  		export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
+# Metadata encription
+export OF_FBE_METADATA_MOUNT_IGNORE=1
 
-		F=$(find "device" -maxdepth 2 -name "fleur")
-		# Modify the background color of the startup screen to #000000
-		\cp -fp bootable/recovery/gui/theme/portrait_hdpi/splash.xml "$F"/recovery/root/twres/splash.xml
-		sed -i 's/value="#D34E38"/value="#000000"/g' "$F"/recovery/root/twres/splash.xml
-		sed -i 's/value="#FF8038"/value="#000000"/g' "$F"/recovery/root/twres/splash.xml
+# Display / Leds
+export OF_SCREEN_H=2340
+export OF_STATUS_H=100
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_HIDE_NOTCH=1
+export OF_CLOCK_POS=1 # left and right clock positions available
+export OF_USE_GREEN_LED=0
+export OF_FLASHLIGHT_ENABLE=0
 
-		echo -e "\x1b[96fleur: When you see this message, all OrangeFox Vars have been added!\x1b[m"
+# Removes the loop block errors after flashing ZIPs (Workaround) 
+export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
 
+# Run a Process After Formatting Data to Work-Around MTP Issues
+export OF_RUN_POST_FORMAT_PROCESS=1
 
-	        # let's see what are our build VARs
-	        if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	           export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	           export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	           export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	           export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	        fi	    
-         fi
+# Other OrangeFox configs
+export FOX_DELETE_AROMAFM=1
+export OF_PATCH_AVB20=1
+export FOX_REPLACE_BUSYBOX_PS="0"
+
+  # let's see what are our build VARs
+   if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+	export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+	export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+	export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+	export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi    
+ fi
 fi
