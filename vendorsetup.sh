3da7f5ed268bbce759b45e29d2963acbe4bb6d13
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-03-07 03:25:03 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -43,7 +43,7 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 	  	
 # Version / Maintainer infos
 export OF_MAINTAINER="Tapin Recovery Instraller"
-export FOX_VERSION=R12.1_1
+export FOX_VERSION=R12.1_0
 export FOX_VARIANT=S-Vendor
 export FOX_BUILD_TYPE="Unofficial"
 export TARGET_DEVICE_ALT="lancelot"
