53b92addc31b1d08c0280630820799a7e53ce196
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-03-21 05:07:10 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -42,71 +42,90 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
  if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	  	
 # Version / Maintainer infos
-export OF_MAINTAINER="Tapin Recovery Instraller"
-export FOX_VERSION=R12.1_0
-export FOX_BUILD_TYPE="Unofficial"
-export TARGET_DEVICE_ALT="lancelot"
-export FOX_TARGET_DEVICES="lancelot,shiva"
+		export OF_MAINTAINER="Tapin Recovery Instraller"
+		export FOX_VERSION=R12.1_0
+		export FOX_BUILD_TYPE="Unofficial"
+	 	export FOX_TARGET_DEVICES="angelica,angelicain,angelican,blossom,cattail,dandelion"
+	  	export TARGET_DEVICE_ALT="angelica,angelican,angelicain,cattail,dandelion"
+	  	export LC_ALL="C"
 
-export TW_DEFAULT_LANGUAGE="en"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-export OF_QUICK_BACKUP_LIST="/boot;/data;"
-export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+	 	# Magiskboot
+	  	export OF_USE_MAGISKBOOT=1
+	 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+  
+		# OTA / DM-Verity / Encryption
+  		export OF_KEEP_DM_VERITY=1
+	  	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+ 		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+		export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+		export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+  		export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	 	export OF_NO_MIUI_PATCH_WARNING=1
+   		export OF_PATCH_AVB20=1
 
-# Magiskboot
-export OF_USE_MAGISKBOOT=1
-export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+		# MediaTek
+	  	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
+	  	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
 
-# OTA
-export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-export OF_NO_MIUI_PATCH_WARNING=1
+	  	# Display / Leds
+		export OF_SCREEN_H="2400"
+		export OF_STATUS_H="100"
+		export OF_STATUS_INDENT_LEFT=48
+		export OF_STATUS_INDENT_RIGHT=48
+		export OF_HIDE_NOTCH=1
+		export OF_CLOCK_POS=1 # left and right clock positions available
+		export OF_USE_GREEN_LED=0
 
-# Binaries
-export FOX_USE_BASH_SHELL=1
-export FOX_ASH_IS_BASH=1
-export FOX_USE_NANO_EDITOR=1
-export FOX_USE_GREP_BINARY=1
-export FOX_USE_TAR_BINARY=1
-export FOX_USE_ZIP_BINARY=1
-export FOX_USE_BRX_BINARY=1
-export FOX_USE_SED_BINARY=1
-export FOX_REPLACE_TOOLBOX_GETPROP=1
-export FOX_USE_XZ_UTILS=1
-export FOX_REPLACE_BUSYBOX_PS=1
-export OF_ENABLE_LPTOOLS=1
+    		# Ensure that /sdcard is bind-unmounted before f2fs data repair or format
+ 		export OF_UNBIND_SDCARD_F2FS=1
 
-# Ensure that /sdcard is bind-unmounted before f2fs data repair or format
-export OF_UNBIND_SDCARD_F2FS=1
+	 	# Metadata encription
+   		export OF_FBE_METADATA_MOUNT_IGNORE=1
 
-# Metadata encription
-export OF_FBE_METADATA_MOUNT_IGNORE=1
+	  	# Removes the loop block errors after flashing ZIPs (Workaround) 
+		export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+		export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
+  
+	  	# Other OrangeFox configs
+		export OF_ENABLE_LPTOOLS=1
+		export OF_ALLOW_DISABLE_NAVBAR=0
+		export FOX_DELETE_AROMAFM=1
+		export FOX_USE_TAR_BINARY=1
+		export FOX_USE_SED_BINARY=1
+		export FOX_USE_XZ_UTILS=1
+                
+	        # Backup
+		export OF_USE_TWRP_SAR_DETECT=1
+		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+		export OF_QUICK_BACKUP_LIST="/boot;/recovery;/data;"
+		export FOX_REPLACE_TOOLBOX_GETPROP=1
+  		export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
 
-# Display / Leds
-export OF_SCREEN_H=2340
-export OF_STATUS_H=100
-export OF_STATUS_INDENT_LEFT=48
-export OF_STATUS_INDENT_RIGHT=48
-export OF_HIDE_NOTCH=1
-export OF_CLOCK_POS=1 # left and right clock positions available
-export OF_USE_GREEN_LED=0
-export OF_FLASHLIGHT_ENABLE=0
+   		# Magisk
+		function download_magisk(){
+		# Usage: download_magisk <destination_path>
+		local DEST=$1
+		if [ -n "${DEST}" ]; then
+			if [ ! -e ${DEST} ]; then
+				echo "Downloading the Latest Release of Magisk..."
+				local LATEST_MAGISK_URL=$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep browser_download_url | grep Magisk- | cut -d : -f 2,3 | tr -d '"')
+				mkdir -p $(dirname ${DEST})
+				wget -q ${LATEST_MAGISK_URL} -O ${DEST} || wget ${LATEST_MAGISK_URL} -O ${DEST}
+				local RCODE=$?
+				if [ "$RCODE" = "0" ]; then
+					echo "Successfully Downloaded Magisk to ${DEST}!"
+					echo "Done!"
+				else
+					echo "Failed to Download Magisk to ${DEST}!"
+				fi
+			fi
+		fi
+		}
+		export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
+		download_magisk $FOX_USE_SPECIFIC_MAGISK_ZIP
 
-# Removes the loop block errors after flashing ZIPs (Workaround) 
-export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
-export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
-
-# Run a Process After Formatting Data to Work-Around MTP Issues
-export OF_RUN_POST_FORMAT_PROCESS=1
-
-# Other OrangeFox configs
-export FOX_DELETE_AROMAFM=1
-export OF_PATCH_AVB20=1
-export FOX_REPLACE_BUSYBOX_PS="1"
+		# maximum permissible splash image size (in kilobytes); do *NOT* increase!
+		export OF_SPLASH_MAX_SIZE=130
 
 F=$(find "device" -maxdepth 2 -name "lancelot")
 # Modify the background color of the startup screen to #000000
